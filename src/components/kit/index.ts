78d245fa1ce1e85e163651592f39566382bc6405
Marco <m.ripamonti@campus.unimib.it>
2021-08-15 15:35:50 +0200

    Add uploader component

---
+++ src/components/kit/index.ts
@@ -0,0 +1,5 @@
+import Uploader from './uploader/uploader';
+import Table from './table/table/table';
+import InlineInput from './inline-input/inline-input';
+
+export { Uploader, Table, InlineInput };
---
+++ src/components/kit/uploader/uploader.tsx
@@ -0,0 +1,45 @@
+import { Button } from '@components/core';
+import { Chip } from '@material-ui/core';
+import { ChangeEvent, useRef } from 'react';
+import PublishRoundedIcon from '@material-ui/icons/PublishRounded';
+import DescriptionRoundedIcon from '@material-ui/icons/DescriptionRounded';
+import styles from './uploader.module.scss';
+
+interface IUploaderProps {
+  selectedFile: File | undefined,
+  handleChangeFile: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+/**
+ * File uploader
+ */
+const Uploader = ({ selectedFile, handleChangeFile }: IUploaderProps) => {
+  // ref to hidden input file
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
+  // handle click on uploader
+  const handleClick = () => {
+    inputFileRef.current?.click();
+  };
+
+  return (
+    <>
+      <div className={styles['button-container']}>
+        <Button onClick={handleClick}>
+          Choose file
+        </Button>
+        <Chip
+          icon={<DescriptionRoundedIcon />}
+          label={selectedFile ? `Selected file: ${selectedFile.name}` : 'No file selected'}
+        />
+      </div>
+      <div role="button" tabIndex={0} onClick={handleClick} onKeyDown={handleClick} className={styles.container}>
+        <PublishRoundedIcon className={styles.icon} />
+        <p>Drop a table here</p>
+      </div>
+      <input onChange={handleChangeFile} ref={inputFileRef} type="file" name="file" className={styles['input-file']} />
+    </>
+  );
+};
+
+export default Uploader;
