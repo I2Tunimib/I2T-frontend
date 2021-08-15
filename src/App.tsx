4e9fe2570396a265f71d326e1cc3b7820fb6c410
Marco <m.ripamonti@campus.unimib.it>
2021-08-15 15:38:31 +0200

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
+import { RouteContainer, Toolbar } from '@components/layout';
+import { IRoute } from '@components/layout/route-container/interfaces';
+import { useFetch } from '@hooks/fetch';
+import { HomepageContainer } from '@pages/homepage';
+import { TableViewer } from '@pages/table-viewer';
+import { IConfigResponse, servicesConfigEndpoint } from '@services/api/endpoints/services-config';
+import { setConfig } from '@store/config/config.slice';
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
+/**
+ * Define routes
+ */
+const routes: IRoute[] = [
+  {
+    path: '/',
+    exact: true,
+    Component: HomepageContainer
+  },
+  {
+    path: '/table/:name',
+    exact: false,
+    Component: TableViewer
+  }
+];
+
+const App = () => {
+  // fetch configuration file
+  useFetch<IConfigResponse>(
+    servicesConfigEndpoint(),
+    {
+      mappingFn: (res) => ({ servicesConfig: res.data }),
+      dispatchFn: setConfig
+    }
   );
-}
+
+  return (
+    <RouteContainer routes={routes} />
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
@@ -9,7 +10,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
