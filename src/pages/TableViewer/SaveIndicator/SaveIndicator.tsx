853bf855eb31d3653a443d6f632c4153cda65d83
Marco <m.ripamonti@campus.unimib.it>
2021-09-04 11:54:28 +0200

    Add save indicator status

--- src/pages/TableViewer/SaveIndicator/SaveIndicator.tsx
+++ src/pages/TableViewer/SaveIndicator/SaveIndicator.tsx
@@ -1,6 +1,12 @@
-import { FC, HTMLAttributes, useEffect } from 'react';
+import { FC, HTMLAttributes, useState } from 'react';
 import CloudOffIcon from '@material-ui/icons/CloudOff';
-import { IconButton, Typography } from '@material-ui/core';
+import LoopRoundedIcon from '@material-ui/icons/LoopRounded';
+import CloudDoneRoundedIcon from '@material-ui/icons/CloudDoneRounded';
+import {
+  IconButton, Theme,
+  Tooltip, Typography,
+  withStyles
+} from '@material-ui/core';
 import clsx from 'clsx';
 import { useAppSelector } from '@hooks/store';
 import { selectLastChangeDate } from '@store/slices/table/table.selectors';
@@ -9,17 +15,113 @@ import styles from './SaveIndicator.module.scss';
 import timeAgoFormatter from './time-formatter';
 
 interface SaveIndicatorProps extends HTMLAttributes<HTMLDivElement> { }
+enum TooltipStatus {
+  ERROR,
+  PENDING,
+  UNSAVED,
+  SAVED
+}
+interface TooltipContentProps extends HTMLAttributes<HTMLDivElement> {
+  status: TooltipStatus;
+}
+
+const LightTooltip = withStyles((theme: Theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: 'rgba(0, 0, 0, 0.87)',
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+    padding: 0
+  }
+}))(Tooltip);
+
+const TooltipContent: FC<TooltipContentProps> = ({
+  status
+}) => {
+  return (
+    <div className={styles.Column}>
+      <div className={clsx(
+        styles.BaseTooltipHeader,
+        {
+          [styles.UnsavedChanges]: status === TooltipStatus.UNSAVED,
+          [styles.SavedChanges]: status === TooltipStatus.SAVED
+        }
+      )}>
+        {status === TooltipStatus.UNSAVED
+          ? <CloudOffIcon className={styles.TooltipIcon} />
+          : <CloudDoneRoundedIcon className={styles.TooltipIcon} />
+        }
+        <Typography variant="subtitle2">
+          {status === TooltipStatus.UNSAVED
+            ? (
+              'You have unsaved changes'
+            )
+            : (
+              'Your changes are up to date'
+            )
+          }
+        </Typography>
+      </div>
+      <div className={styles.BaseTooltipBody}>
+        <Typography variant="caption">
+          {status === TooltipStatus.UNSAVED
+            ? (
+              'You have changes which have not been saved yet. Save the document to keep all changes.'
+            )
+            : (
+              'Your latest changes are now saved. Your work can be closed safely.'
+            )
+          }
+        </Typography>
+      </div>
+    </div>
+  );
+};
 
 const SaveIndicator: FC<SaveIndicatorProps> = ({
   className
 }) => {
+  const [status, setStatus] = useState<TooltipStatus>(TooltipStatus.UNSAVED);
   const lastChange = useAppSelector(selectLastChangeDate);
+  // substitute with selector for http request
+  const loading = false;
 
   return (
     <div className={styles.Container}>
-      <IconButton size="small" className={className}>
-        <CloudOffIcon className={styles.Icon} />
-      </IconButton>
+      <LightTooltip
+        title={(
+          <TooltipContent status={status} />
+        )}>
+        <IconButton size="small" className={className}>
+          {loading
+            ? (
+              <LoopRoundedIcon className={clsx(
+                styles.Icon,
+                styles.Loading
+              )} />
+            )
+            : [
+              status === TooltipStatus.SAVED
+                ? (
+                  <CloudDoneRoundedIcon
+                    key="saved"
+                    className={clsx(
+                      styles.Icon,
+                      styles.SavedChanges
+                    )} />
+                )
+                : (
+                  <CloudOffIcon
+                    key="unsaved"
+                    className={clsx(
+                      styles.Icon,
+                      styles.UnsavedChanges
+                    )} />
+                )
+            ]
+          }
+        </IconButton>
+      </LightTooltip>
       <Typography className={styles.LastChange} color="textSecondary" variant="body2">
         {lastChange
           ? (
