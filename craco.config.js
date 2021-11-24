1622ebe7e89c5a696bbd7eff6a68a698b73416e3
Marco Ripamonti <m.ripamonti@campus.unimib.it>
2021-11-24 12:13:17 +0100

    Add purgeCss to remove unused css at build time

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,26 @@
 const CracoAlias = require('craco-alias');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const { addBeforeLoader, loaderByName } = require('@craco/craco');
 
 module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      const yamlLoader = {
-        test: /\.ya?ml$/,
-        use: 'js-yaml-loader'
-      };
-      addBeforeLoader(webpackConfig, loaderByName('file-loader'), yamlLoader);
+  // webpack: {
+  //   configure: (webpackConfig) => {
+  //     const yamlLoader = {
+  //       test: /\.ya?ml$/,
+  //       use: 'js-yaml-loader'
+  //     };
+  //     addBeforeLoader(webpackConfig, loaderByName('file-loader'), yamlLoader);
 
-      return webpackConfig;
+  //     return webpackConfig;
+  //   }
+  // },
+  style: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts']
+        })
+      ]
     }
   },
   plugins: [
