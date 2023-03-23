56ab0238a0c46dae30e5aaae7e254fad5c1425ec
Roberto <r.avogadro@campus.unimib.it>
2023-03-23 17:22:24 +0100

    change georss uri

--- src/services/utils/kg-info.ts
+++ src/services/utils/kg-info.ts
@@ -9,7 +9,7 @@ export const KG_INFO = {
     uri: 'https://www.wikidata.org/wiki/'
   },
   georss: {
-    uri: 'http://149.132.178.112:3002/map?polyline='
+    uri: 'http://149.132.178.149:3002/map?polyline='
   },
   atoka: {
     uri: 'https://atoka.io/public/en/company/-/'
