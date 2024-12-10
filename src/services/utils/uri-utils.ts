7e8f7b1f724f1d942fcd62b61a0f9db53c0c6735
Manuel Elia <manuel.elia80@gmail.com>
2024-12-10 17:15:22 +0100

    Updated EntityTab and PropertyTab with add and remove button

---
+++ src/services/utils/uri-utils.ts
@@ -0,0 +1,39 @@
+type QueryParams = {
+    [key: string]: string | number | undefined;
+};
+
+// Function to build a URI
+export const buildURI = (baseUrl : string, path : string, queryParams? : QueryParams)  => {
+    const url = new URL(path, baseUrl);
+
+    if (queryParams) {
+        const searchParams = new URLSearchParams();
+
+        // Append each key-value pair to the search parameters
+        Object.keys(queryParams).forEach(key => {
+            const value = queryParams[key];
+            if (value !== undefined) {
+                searchParams.append(key, String(value)); // Convert numbers to strings
+            }
+        });
+
+        url.search = searchParams.toString();
+    }
+console.log('uri built: ' + url.toString());
+    return url.toString();
+}
+/*
+// Example usage:
+const baseUrl = 'https://api.example.com';
+const path = '/products';
+const queryParams: QueryParams = {
+    category: 'electronics',
+    sort: 'price',
+    page: 3,
+    limit: 20,
+    search: undefined // Example of an undefined value (which will be ignored)
+};
+
+const fullURI = buildURI(baseUrl, path, queryParams);
+console.log(fullURI); // Outputs: https://api.example.com/products?category=electronics&sort=price&page=3&limit=20
+*/
