24262b94d97e6ad23c094cc63f7bf5fcb2cf955a
Blowdire <agazzi.ruben99@gmail.com>
2025-05-16 08:51:37 +0200

    refine matching dialog help

--- src/pages/Viewer/TableViewer/RefineMatching/RefineMatchingDialog.tsx
+++ src/pages/Viewer/TableViewer/RefineMatching/RefineMatchingDialog.tsx
@@ -1,8 +1,20 @@
-import { MenuBase } from '@components/core';
-import { Box, PopperPlacementType, Stack, Tab, Tabs, Typography } from '@mui/material';
-import { FC, ReactNode, SyntheticEvent, useState } from 'react';
-import ScoreRefineMatching from './ScoreRefineMatching';
-import TypeRefineMatching from './TypeRefineMatching';
+import { MenuBase } from "@components/core";
+import {
+  Box,
+  IconButton,
+  PopperPlacementType,
+  Stack,
+  Tab,
+  Tabs,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { FC, ReactNode, SyntheticEvent, useState } from "react";
+import ScoreRefineMatching from "./ScoreRefineMatching";
+import TypeRefineMatching from "./TypeRefineMatching";
+import { HelpOutlineRounded } from "@mui/icons-material";
+import { useAppDispatch } from "@hooks/store";
+import { updateUI } from "@store/slices/table/table.slice";
 
 export type RefineMatchingProps = {
   open: boolean;
@@ -10,13 +22,13 @@ export type RefineMatchingProps = {
   handleClose: () => void;
   id?: string;
   placement?: PopperPlacementType | undefined;
-}
+};
 
 type TabPanelProps = {
   children?: ReactNode;
   index: number;
   value: number;
-}
+};
 
 const TabPanel = (props: TabPanelProps) => {
   const { children, value, index, ...other } = props;
@@ -29,11 +41,7 @@ const TabPanel = (props: TabPanelProps) => {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <>
-          {children}
-        </>
-      )}
+      {value === index && <>{children}</>}
     </div>
   );
 };
@@ -41,25 +49,55 @@ const TabPanel = (props: TabPanelProps) => {
 const RefineMatchingDialog: FC<RefineMatchingProps> = ({
   open,
   anchorElement,
-  handleClose
+  handleClose,
 }) => {
   const [value, setValue] = useState(0);
+  const dispatch = useAppDispatch();
 
   const handleChange = (event: SyntheticEvent, newValue: number) => {
     setValue(newValue);
+  };
+
+  const handleHelpClick = () => {
+    dispatch(updateUI({ openHelpDialog: true, tutorialStep: 7 }));
+    handleClose();
   };
 
   return (
     <MenuBase
       open={open}
       anchorElement={anchorElement}
-      handleClose={handleClose}>
-      <Box sx={{ width: '100%' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+      handleClose={handleClose}
+    >
+      <Box sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
             <Tab label="Type Refine Matching" />
             <Tab label="Score Refine Matching" />
           </Tabs>
+          <Tooltip title="Help">
+            <IconButton
+              sx={{
+                color: "rgba(0, 0, 0, 0.54)",
+                marginRight: "8px",
+              }}
+              onClick={handleHelpClick}
+            >
+              <HelpOutlineRounded />
+            </IconButton>
+          </Tooltip>
         </Box>
         <TabPanel value={value} index={0}>
           <TypeRefineMatching handleClose={handleClose} />
