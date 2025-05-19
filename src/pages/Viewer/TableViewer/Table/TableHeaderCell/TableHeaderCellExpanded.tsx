2fc5f77bb1b18c927c5046894cc1e11960b8cdb8
Blowdire <agazzi.ruben99@gmail.com>
2025-05-19 09:25:36 +0200

    check for some wikidata blank urls

--- src/pages/Viewer/TableViewer/Table/TableHeaderCell/TableHeaderCellExpanded.tsx
+++ src/pages/Viewer/TableViewer/Table/TableHeaderCell/TableHeaderCellExpanded.tsx
@@ -1,12 +1,17 @@
-import { Chip, Link, Typography } from '@mui/material';
-import { ID } from '@store/interfaces/store';
-import { ColumnMetadata, Context } from '@store/slices/table/interfaces/table';
-import { FC, useCallback } from 'react';
-import ArrowRightAltRoundedIcon from '@mui/icons-material/ArrowRightAltRounded';
-import EntityLabel from '@components/core/EntityLabel';
-import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
-import { ExpandableList, ExpandableListBody, ExpandableListHeader, ExpandableListItem } from '@components/kit';
-import styles from './TableHeaderCellExpanded.module.scss';
+import { Chip, Link, Typography } from "@mui/material";
+import { ID } from "@store/interfaces/store";
+import { ColumnMetadata, Context } from "@store/slices/table/interfaces/table";
+import { FC, useCallback } from "react";
+import ArrowRightAltRoundedIcon from "@mui/icons-material/ArrowRightAltRounded";
+import EntityLabel from "@components/core/EntityLabel";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
+import {
+  ExpandableList,
+  ExpandableListBody,
+  ExpandableListHeader,
+  ExpandableListItem,
+} from "@components/kit";
+import styles from "./TableHeaderCellExpanded.module.scss";
 
 interface TableHeaderCellExpandedProps {
   context: Record<ID, Context>;
@@ -19,48 +24,67 @@ const TableHeaderCellExpanded: FC<TableHeaderCellExpandedProps> = ({
   context,
   metadata,
   kind,
-  role
+  role,
 }) => {
-  const getResourceLink = useCallback((idResource) => {
-    const [prefix, id] = idResource.split(':');
-    const resourceContext = context[prefix];
-    if (resourceContext) {
-      return `${resourceContext.uri}${id}`;
-    }
-    return '';
-  }, [context]);
+  const getResourceLink = useCallback(
+    (idResource) => {
+      const [prefix, id] = idResource.split(":");
+      const regexForWikidata = /^Q\d+$/;
+      const resourceContext = context[prefix];
+      if (resourceContext) {
+        return `${resourceContext.uri}${id}`;
+      } else if (regexForWikidata.test(idResource)) {
+        return `https://www.wikidata.org/wiki/${idResource}`;
+      }
+      return "";
+    },
+    [context]
+  );
 
-  const RenderType = useCallback((type) => (
-    <div key={type.id} className={styles.PropertyItem}>
-      <EntityLabel type="type">
-        <Link href={getResourceLink(type.id)} target="_blank">{type.name}</Link>
-      </EntityLabel>
-    </div>
-  ), []);
+  const RenderType = useCallback(
+    (type) => (
+      <div key={type.id} className={styles.PropertyItem}>
+        <EntityLabel type="type">
+          <Link href={getResourceLink(type.id)} target="_blank">
+            {type.name}
+          </Link>
+        </EntityLabel>
+      </div>
+    ),
+    []
+  );
 
-  const RenderProperty = useCallback((property) => (
-    <div key={property.id} className={styles.PropertyItem}>
-      <EntityLabel type="property">
-        <Link href={getResourceLink(property.id)} target="_blank">{property.name}</Link>
-      </EntityLabel>
-      <ArrowRightAltRoundedIcon />
-      <span>{property.obj}</span>
-    </div>
-  ), []);
+  const RenderProperty = useCallback(
+    (property) => (
+      <div key={property.id} className={styles.PropertyItem}>
+        <EntityLabel type="property">
+          <Link href={getResourceLink(property.id)} target="_blank">
+            {property.name}
+          </Link>
+        </EntityLabel>
+        <ArrowRightAltRoundedIcon />
+        <span>{property.obj}</span>
+      </div>
+    ),
+    []
+  );
 
-  const getItems = useCallback((start: number, finish: number): any[] => {
-    if (!metadata[0]) {
-      return [];
-    }
-    if (!metadata[0].entity) {
-      return [];
-    }
-    let end = finish;
-    if (finish > metadata[0].entity.length) {
-      end = metadata[0].entity.length;
-    }
-    return metadata[0].entity.slice(start, end);
-  }, [metadata]);
+  const getItems = useCallback(
+    (start: number, finish: number): any[] => {
+      if (!metadata[0]) {
+        return [];
+      }
+      if (!metadata[0].entity) {
+        return [];
+      }
+      let end = finish;
+      if (finish > metadata[0].entity.length) {
+        end = metadata[0].entity.length;
+      }
+      return metadata[0].entity.slice(start, end);
+    },
+    [metadata]
+  );
 
   const nEntities = metadata[0]
     ? metadata[0].entity
@@ -75,7 +99,7 @@ const TableHeaderCellExpanded: FC<TableHeaderCellExpandedProps> = ({
 
   return metadata.length > 0 ? (
     <div className={styles.Container}>
-      {(metadata[0].type && Array.isArray(metadata[0].type)) && (
+      {metadata[0].type && Array.isArray(metadata[0].type) && (
         <div className={styles.PropertyContainer}>
           {metadata[0].type.map((type) => RenderType(type))}
         </div>
@@ -87,20 +111,24 @@ const TableHeaderCellExpanded: FC<TableHeaderCellExpandedProps> = ({
       )}
       <ExpandableList
         messageIfNoContent="Cell doesn't have any entity metadata"
-        className={styles.ExpandableList}>
+        className={styles.ExpandableList}
+      >
         <ExpandableListHeader>
           {getItems(0, 3).map((item, index) => (
             <ExpandableListItem key={`${item.id}`}>
               <div className={styles.Item}>
-                {item.match ? <CheckRoundedIcon className={styles.Icon} /> : null}
+                {item.match ? (
+                  <CheckRoundedIcon className={styles.Icon} />
+                ) : null}
                 <EntityLabel className={styles.MetaLink} type="entity">
                   <Link
                     sx={{
-                      marginLeft: '20px'
+                      marginLeft: "20px",
                     }}
                     title={`${item.id} (${item.name.value})`}
                     href={item.url}
-                    target="_blank">
+                    target="_blank"
+                  >
                     {`${item.id} (${item.name.value})`}
                   </Link>
                 </EntityLabel>
@@ -112,16 +140,19 @@ const TableHeaderCellExpanded: FC<TableHeaderCellExpandedProps> = ({
           {getItems(3, nEntities).map((item, index) => (
             <ExpandableListItem key={`${item.id}`}>
               <div className={styles.Item}>
-                {item.match ? <CheckRoundedIcon className={styles.Icon} /> : null}
+                {item.match ? (
+                  <CheckRoundedIcon className={styles.Icon} />
+                ) : null}
                 <EntityLabel className={styles.MetaLink} type="entity">
                   <Link
                     sx={{
-                      marginLeft: '20px'
+                      marginLeft: "20px",
                     }}
                     href={item.url}
                     title={`${item.id} (${item.name.value})`}
                     target="_blank"
-                    className={styles.MetaLink}>
+                    className={styles.MetaLink}
+                  >
                     {`${item.id} (${item.name.value})`}
                   </Link>
                 </EntityLabel>
