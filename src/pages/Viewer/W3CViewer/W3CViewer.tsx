5290984f5629af2300a5168d2f6ea0f8bea4762c
Blowdire <agazzi.ruben99@gmail.com>
2025-06-16 09:49:36 +0200

    fixed json view

--- src/pages/Viewer/W3CViewer/W3CViewer.tsx
+++ src/pages/Viewer/W3CViewer/W3CViewer.tsx
@@ -1,27 +1,28 @@
-import useIsMounted from '@hooks/is-mounted/useIsMounted';
-import { useAppDispatch } from '@hooks/store';
-import { exportTable } from '@store/slices/table/table.thunk';
-import { useEffect, useState } from 'react';
-import ReactJson from 'react-json-view';
-import { useParams } from 'react-router-dom';
-import styles from './W3CViewer.module.scss';
+import useIsMounted from "@hooks/is-mounted/useIsMounted";
+import { useAppDispatch } from "@hooks/store";
+import { exportTable } from "@store/slices/table/table.thunk";
+import { useEffect, useState } from "react";
+import ReactJson from "react-json-view";
+import { useParams } from "react-router-dom";
+import styles from "./W3CViewer.module.scss";
 
 const W3CViewer = () => {
   const [data, setData] = useState<any>();
   const isMounted = useIsMounted();
-  const params = useParams<{ datasetId: string, tableId: string }>();
+  const params = useParams<{ datasetId: string; tableId: string }>();
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     let mounted = true;
-    dispatch(exportTable({ format: 'W3C', params }))
-      .unwrap()
-      .then((res) => {
-        if (mounted) {
-          setData(res);
-        }
-      });
+    // dispatch(exportTable({ format: "W3C", params }))
+    //   .unwrap()
+    //   .then((res) => {
+    //     if (mounted) {
+    //       setData(res);
+    //     }
+    //   });
+    convertToW3C();
     // dispatch(convertToW3C(false))
     //   .unwrap()
     //   .then((res) => {
@@ -29,35 +30,39 @@ const W3CViewer = () => {
     //       setData(res);
     //     }
     //   });
-    return () => { mounted = false; };
+    return () => {
+      mounted = false;
+    };
   }, []);
-
+  async function convertToW3C() {
+    const res = await dispatch(
+      exportTable({ format: "JSON (W3C Compliant)", params })
+    ).unwrap();
+    setData(res);
+  }
   return (
     <div className={styles.Container}>
       <ReactJson
         src={data}
         enableClipboard={false}
-        shouldCollapse={({
-          name, src,
-          type, namespace
-        }) => {
-          const path = namespace.join('.');
-          if (path === 'root') {
+        shouldCollapse={({ name, src, type, namespace }) => {
+          const path = namespace.join(".");
+          if (path === "root") {
             return false;
           }
-          if (path === 'root.0' || path === 'root.1') {
+          if (path === "root.0" || path === "root.1") {
             return false;
           }
-          if (name === 'th0' || name === 'Point of interest') {
+          if (name === "th0" || name === "Point of interest") {
             return false;
           }
-          if (name === 'metadata' || name === 'Point of interest') {
+          if (name === "metadata" || name === "Point of interest") {
             return false;
           }
-          if (name === '0' || name === '0') {
+          if (name === "0" || name === "0") {
             return false;
           }
-          if (name === 'property') {
+          if (name === "property") {
             return false;
           }
           return true;
