d0ed3b540c8e13b61b79c07b38a7253d7af50c77
Blowdire <agazzi.ruben99@gmail.com>
2025-10-29 16:41:53 +0100

    updates for lion linker prefix

--- src/services/utils/regexs.ts
+++ src/services/utils/regexs.ts
@@ -1,9 +1,11 @@
 export const dateRegex = new RegExp(
-  "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$"
+  "^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$",
 );
 export const stripWikidataPrefix = (str: string): string => {
   if (str.startsWith("wd:")) {
     return str.slice(3);
+  } else if (str.startsWith("wdL:")) {
+    return str.slice(4);
   } else if (str.startsWith("wdA:")) {
     return str.slice(4);
   } else if (str.startsWith("wda:")) {
