8bbc19be646e699d6c97059c5c478752bb0fc60b
Blowdire <agazzi.ruben99@gmail.com>
2026-02-03 09:27:49 +0100

    updated socket configuration to prevent errors if ws:// is blocked by firewall configurations

--- src/components/core/SocketIoProvider/SocketIoProvider.tsx
+++ src/components/core/SocketIoProvider/SocketIoProvider.tsx
@@ -1,15 +1,23 @@
-import { FC, useEffect, useState, useContext } from 'react';
-import io, { Socket } from 'socket.io-client';
-import SocketIoContext from './SocketIoContext';
+import { FC, useEffect, useState, useContext } from "react";
+import io, { Socket } from "socket.io-client";
+import SocketIoContext from "./SocketIoContext";
 
 const SocketIoProvider: FC<{}> = ({ children }) => {
   const [state, setState] = useState<Socket>();
 
   useEffect(() => {
-    const socket = io(import.meta.env.VITE_BACKEND_DOMAIN || '', { transports: ['websocket'] });
+    const socket = io(import.meta.env.VITE_BACKEND_DOMAIN || "", {
+      transports: ["websocket", "polling"],
+      upgrade: true,
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionAttempts: 5,
+    });
     setState(socket);
 
-    return () => { socket.disconnect(); };
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   return (
