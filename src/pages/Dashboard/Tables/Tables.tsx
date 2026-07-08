d2bff5865eb5875157bba61de63a6f998f709fab
Blowdire <agazzi.ruben99@gmail.com>
2026-07-08 09:07:31 +0200

    merge problem fix

--- src/pages/Dashboard/Tables/Tables.tsx
+++ src/pages/Dashboard/Tables/Tables.tsx
@@ -6,14 +6,6 @@ import {
   useReactTable,
   getCoreRowModel,
   getPaginationRowModel,
-} from '@tanstack/react-table';
-import { useAppDispatch, useAppSelector } from '@hooks/store';
-import { ReadMoreRounded, AssignmentTurnedInOutlined, AccountTreeRounded, LockOutlined, LockOpenOutlined, ShareOutlined } from '@mui/icons-material';
-import { updateUI } from '@store/slices/table/table.slice';
-import { getTable, getDependencies } from '@store/slices/table/table.thunk';
-import ComplianceDialog from '@pages/Viewer/TableViewer/ComplianceDialog';
-import GraphDialog from '@pages/Viewer/TableViewer/GraphDialog';
-import DependenciesPanel from '@pages/Viewer/TableViewer/DependenciesPanel';
 } from "@tanstack/react-table";
 import { useAppDispatch, useAppSelector } from "@hooks/store";
 import {
@@ -22,11 +14,13 @@ import {
   AccountTreeRounded,
   LockOutlined,
   LockOpenOutlined,
+  ShareOutlined,
 } from "@mui/icons-material";
 import { updateUI } from "@store/slices/table/table.slice";
 import { getTable, getDependencies } from "@store/slices/table/table.thunk";
 import { selectComplianceDialogStatus } from "@store/slices/table/table.selectors";
 import ComplianceDialog from "@pages/Viewer/TableViewer/ComplianceDialog";
+import GraphDialog from "@pages/Viewer/TableViewer/GraphDialog";
 import DependenciesPanel from "@pages/Viewer/TableViewer/DependenciesPanel";
 import {
   Button,
@@ -38,15 +32,8 @@ import {
   Stack,
   Tooltip,
   Typography,
-} from '@mui/material';
-import { ID } from '@store/interfaces/store';
-import {
-  selectCurrentDatasetTables,
-  selectGetTablesDatasetStatus,
-  selectDatasets,
-} from '@store/slices/datasets/datasets.selectors';
-import { selectIsLoggedIn } from "@store/slices/auth/auth.selectors";
-import { getTablesByDataset } from '@store/slices/datasets/datasets.thunk';
+} from "@mui/material";
+import { ID } from "@store/interfaces/store";
 import {
   selectCurrentDatasetTables,
   selectGetTablesDatasetStatus,
@@ -234,35 +221,34 @@ const Tables: FC<TablesProps> = ({ onSelectionChange, viewType }) => {
             <IconButton
               color="primary"
               size="small"
-              variant="contained"
-              color="primary"
-              startIcon={isLoadingTableData ? <CircularProgress size={14} color="inherit" /> : <ShareOutlined />}
-              disabled={isLoadingTableData}
-              onClick={async () => {
-                setSelectedTableId(row.original.id);
-                setIsLoadingTableData(true);
-                try {
-                  await dispatch(getTable({ tableId: row.original.id, datasetId })).unwrap();
-                } catch {
-                }
-                setIsLoadingTableData(false);
-                dispatch(updateUI({ openGraphDialog: true }));
-              }}>
-              Schema
-            </Button>
+              component={Link}
+              to={`/datasets/${datasetId}/tables/${row.original.id}?view=${viewMode}`}
+            >
+              <ReadMoreRounded />
+            </IconButton>
+          ) : (
             <Button
               size="small"
-              variant="outlined"
-              color="primary"
-              startIcon={isLoadingDeps ? <CircularProgress size={14} color="inherit" /> : <AccountTreeRounded />}
-              disabled={isLoadingDeps}
-              onClick={async () => {
-                setIsLoadingDeps(true);
-                try {
-                  await dispatch(getTable({ tableId: row.original.id, datasetId })).unwrap();
-                  await dispatch(getDependencies({ tableId: row.original.id, datasetId })).unwrap();
-                } catch {
-                  // open panel anyway on error
+              component={Link}
+              to={`/datasets/${datasetId}/tables/${row.original.id}?view=${viewMode}`}
+              endIcon={<ReadMoreRounded />}
+              classes={{ endIcon: globalStyles.IconButton }}
+            >
+              Explore
+            </Button>
+          )}
+          {viewType === "list" && (
+            <>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                startIcon={
+                  isLoadingTableData ? (
+                    <CircularProgress size={14} color="inherit" />
+                  ) : (
+                    <AssignmentTurnedInOutlined />
+                  )
                 }
                 disabled={isLoadingTableData}
                 onClick={async () => {
@@ -281,6 +267,34 @@ const Tables: FC<TablesProps> = ({ onSelectionChange, viewType }) => {
                 }}
               >
                 Compliance
+              </Button>
+              <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                startIcon={
+                  isLoadingTableData ? (
+                    <CircularProgress size={14} color="inherit" />
+                  ) : (
+                    <ShareOutlined />
+                  )
+                }
+                disabled={isLoadingTableData}
+                onClick={async () => {
+                  setSelectedTableId(row.original.id);
+                  setIsLoadingTableData(true);
+                  try {
+                    await dispatch(
+                      getTable({ tableId: row.original.id, datasetId }),
+                    ).unwrap();
+                  } catch {
+                    // open dialog anyway on error
+                  }
+                  setIsLoadingTableData(false);
+                  dispatch(updateUI({ openGraphDialog: true }));
+                }}
+              >
+                Schema
               </Button>
               <Button
                 size="small"
@@ -323,10 +337,16 @@ const Tables: FC<TablesProps> = ({ onSelectionChange, viewType }) => {
 
   return (
     <>
-      <ComplianceDialog datasetId={datasetId} tableId={selectedTableId} />
+      <ComplianceDialog
+        datasetId={datasetId}
+        tableId={selectedTableId}
+        readonly
+      />
       <GraphDialog datasetId={datasetId} tableId={selectedTableId} />
-      <Box sx={{ display: 'flex', flex: 1, overflow: 'hidden', minHeight: 400 }}>
-        <Box sx={{ flex: 1, overflow: 'auto', minWidth: 0 }}>
+      <Box
+        sx={{ display: "flex", flex: 1, overflow: "hidden", minHeight: 400 }}
+      >
+        <Box sx={{ flex: 1, overflow: "auto", minWidth: 0 }}>
           {loading ? (
             <LinearProgress />
           ) : viewType === "list" ? (
